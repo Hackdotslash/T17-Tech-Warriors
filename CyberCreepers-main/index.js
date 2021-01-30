bc7f47016a7f695d0631e4fcf551feb3162b4019
Raj Shah <34474021+rajshah9914@users.noreply.github.com>
2021-01-30 19:50:03 +0530

    Update index.js

--- CyberCreepers-main/index.js
+++ CyberCreepers-main/index.js
@@ -1,6 +1,16 @@
+window.onload = function() {
+    document.querySelectorAll('a').forEach(node => {
+        console.log(node)
+        if (node.target === "_blank") {
+            node.href = "#";
+            node.style.display = "none";
+            console.log("gaya re baba")
+        }
+    })    
+};
+
 let Http = new XMLHttpRequest();
-const url = 'https://boogle-e8231-default-rtdb.firebaseio.com/blacklist.json';
-Http.open("GET", url);
+Http.open("GET", 'https://boogle-e8231-default-rtdb.firebaseio.com/blacklist.json');
 Http.send();
 Http.onreadystatechange = (e) => {
 if (Http.readyState == 4 && Http.status == 200) {
@@ -16,7 +26,9 @@ if (Http.readyState == 4 && Http.status == 200) {
     var cc = blacklist_array.filter(rr => {
         return rr.url === window.location.href
     })
-    if (cc.length == 1) {
+    
+    if (cc.length > 0) {
+        window.stop();
         window.location.href ="https://www.google.com/"
         alert("You have been redirected to this site since the website was blacklisted..")
     }
